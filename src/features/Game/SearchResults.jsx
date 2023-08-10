77e547632c78c00246b75077d742514f4f582a8b
wojtek <wswistak@student.agh.edu.pl>
2023-08-10 23:12:03 +0200

    closing searching results func

--- src/features/Game/SearchResults.jsx
+++ src/features/Game/SearchResults.jsx
@@ -3,15 +3,20 @@ import { Link } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Loader from "../../ui/Loader";
 
-function SearchResults({ query, clearQuery }) {
-  const [isClosed, setIsClosed] = useState(false);
+function SearchResults({
+  query,
+  clearQuery,
+  setHideCloseIcon,
+  isResultsClosed,
+}) {
   const { data, isLoading } = useFetch("games", `search=${query}`);
-
-  if (isClosed) return null;
+  // isResultsClosed = false;
+  if (isResultsClosed) return null;
 
   const handleClosing = () => {
-    setIsClosed(true);
+    isResultsClosed = true;
     clearQuery();
+    setHideCloseIcon(true);
   };
   return (
     <div className=" absolute z-[5]  grid h-max 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3  md:grid-cols-2 gap-5 px-10 top-0  bottom-0 left-0 right-0 bg-[var(--bg-primary)] py-24 ">
--- src/ui/HeaderSearch.jsx
+++ src/ui/HeaderSearch.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import SearchResults from "../features/Game/SearchResults";
+import { TfiClose } from "react-icons/tfi";
+import { Link } from "react-router-dom";
 
 function HeaderInput() {
   const [query, setQuery] = useState("");
+  const [hideCloseIcon, setHideCloseIcon] = useState(true);
+  const [isResultsClosed, setIsResultsClosed] = useState(false);
 
   function handleQuery(newQuery) {
     setQuery(newQuery);
@@ -14,17 +18,34 @@ function HeaderInput() {
 
   return (
     <>
-      <form className="z-10 bg-neutral-700 flex w-1/5 min-w-max items-center rounded-full px-4 transition-all ease-in duration-200 hover:w-2/4 focus-within:w-2/4 ">
+      <form className="z-10 text-[var(--c-gray-light)] text-2xl bg-neutral-700 flex w-1/5 min-w-max items-center rounded-full px-4 transition-all ease-in duration-200 hover:w-2/4 focus-within:w-2/4 ">
         <FiSearch color="var(--c-gray-light)" />{" "}
         <input
           placeholder="Search game"
           className=" placeholder:text-[--c-gray-light] w-full h-11 text-sm focus:outline-none focus:ring-yellow-500 px-2 bg-neutral-700"
           value={query}
-          onChange={(e) => handleQuery(e.target.value)}
+          onChange={(e) => {
+            handleQuery(e.target.value);
+            setIsResultsClosed(false);
+            setHideCloseIcon(false);
+            if (e.target.value === "") setHideCloseIcon(true);
+          }}
         />
+        <Link onClick={() => setIsResultsClosed(true)}>
+          <TfiClose
+            fontWeight="bold"
+            className={`text-xl ${hideCloseIcon ? "invisible" : "visible"}`}
+          />
+        </Link>
       </form>
-      {query.length > 1 && (
-        <SearchResults query={query} clearQuery={clearQuery} />
+      {query.length > 0 && (
+        <SearchResults
+          query={query}
+          clearQuery={clearQuery}
+          setHideCloseIcon={setHideCloseIcon}
+          onClose={() => setHideCloseIcon(true)}
+          isResultsClosed={isResultsClosed}
+        />
       )}
     </>
   );
