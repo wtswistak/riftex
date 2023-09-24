ad6a8d6e68aa1df57c0cc73d40b132bb3a48f232
wojtek <wswistak@student.agh.edu.pl>
2023-09-24 17:26:56 +0200

    circle loader

---
+++ src/ui/CircleLoader.jsx
@@ -0,0 +1,12 @@
+function CircleLoader() {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default CircleLoader;
