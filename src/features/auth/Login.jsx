b96b45040f07b7a9d8a6dc7c6a57047e884efc06
wojtek <wswistak@student.agh.edu.pl>
2023-10-03 14:16:31 +0200

    styles mobile

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -3,8 +3,8 @@ import LoginForm from "./LoginForm";
 
 function Login() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div className="p-16 pt-18 pb-28 bg-[var(--bg-secondary)] rounded-xl">
+    <div className="flex h-screen w-full items-center justify-center ">
+      <div className="p-16 pt-18 pb-28 bg-[var(--bg-secondary)] rounded-xl max-md:h-full max-md:w-full max-md:rounded-none max-sm:px-10">
         <div className="flex justify-center mb-2">
           <Logo isLogoVisible={true} />
         </div>
