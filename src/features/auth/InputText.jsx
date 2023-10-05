1765257cec1b38136100b8c7fc7bad96b26c0098
wojtek <wswistak@student.agh.edu.pl>
2023-10-05 12:21:08 +0200

    capitalize labels

--- src/features/auth/InputText.jsx
+++ src/features/auth/InputText.jsx
@@ -1,12 +1,12 @@
 function InputText({ value, setValue, isLoading, type }) {
   return (
     <>
-      <label htmlFor={type} className="mb-1">
+      <label htmlFor={type} className="mb-1 capitalize">
         {type}
       </label>
       <input
         type={type}
-        id={type}
+        name={type}
         className="rounded-sm mb-3  p-1 bg-[var(--dark-gray)]"
         value={value}
         onChange={(e) => setValue(e.target.value)}
