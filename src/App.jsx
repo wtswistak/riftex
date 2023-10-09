e14eeea3c296ddc30828397af9fdb9af7df899c9
wojtek <wswistak@student.agh.edu.pl>
2023-10-09 13:44:02 +0200

    add global value (hidde sidebar), context

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Account from "./pages/Account";
 import Login from "./features/auth/Login";
 import SignUp from "./pages/SignUp";
+import { SidebarProvider } from "./contexts/SidebarContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,20 +24,22 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Applayout />}>
-            <Route index element={<Homepage />} />
-            <Route path="platforms/:id" element={<Platform />} />
-            <Route path="games/:id" element={<Game />} />
-            <Route path="genres/:id" element={<Genre />} />
-            <Route path="account/:id" element={<Account />} />
-            <Route path="*" element={<NotPageFound />} />
-          </Route>
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <SidebarProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Applayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="platforms/:id" element={<Platform />} />
+              <Route path="games/:id" element={<Game />} />
+              <Route path="genres/:id" element={<Genre />} />
+              <Route path="account/:id" element={<Account />} />
+              <Route path="*" element={<NotPageFound />} />
+            </Route>
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </SidebarProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/contexts/SidebarContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+const SidebarContext = createContext();
+
+const SidebarProvider = ({ children }) => {
+  const [isSidebarHidden, setIsSidebarHidden] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarHidden(!isSidebarHidden);
+  };
+  return (
+    <SidebarContext.Provider value={{ isSidebarHidden, toggleSidebar }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+export { SidebarContext, SidebarProvider };
