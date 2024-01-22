800ed512eb74d2a28b49e09aec0344e470965cc3
wojtek <wswistak@student.agh.edu.pl>
2024-01-22 23:13:55 +0100

    get comments data

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,15 +2,20 @@ import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import GameDetails from "../features/Game/GameDetails";
 import Loader from "../ui/Loader";
+import CommentBox from "../features/Game/CommentBox";
+import { useGetComments } from "../features/Game/useComment";
 
 function Game() {
-  const { id } = useParams();
-  const { data, isLoading } = useFetch(`games/${id}`);
-
+  const { id: gameId } = useParams();
+  const { data, isLoading } = useFetch(`games/${gameId}`);
+  const { commentLoading, commentData } = useGetComments(gameId);
   return (
     <>
-      {isLoading && <Loader />}
-      <GameDetails key={data.id} data={data} />
+      {(isLoading || commentLoading) && <Loader />}
+      <div className="px-20 max-xl:ml-4 max-md:mr-0 max-xl:px-4 max-sm:px-0 max-sm:mx-0">
+        <GameDetails key={data.gameId} data={data} />
+        <CommentBox key={data.gameId} data={commentData} />
+      </div>
     </>
   );
 }
