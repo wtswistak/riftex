9ebdb071a508dadef429d778814ee2d494f6aa81
wojtek <wswistak@student.agh.edu.pl>
2024-02-13 14:31:51 +0100

    rename

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import GameDetails from "../features/Game/GameDetails";
 import Loader from "../ui/Loader";
-import CommentBox from "../features/Game/CommentBox";
+import CommentsBox from "../features/Game/CommentsBox";
 import { useGetComments } from "../features/Game/useComment";
 
 function Game() {
@@ -15,7 +15,7 @@ function Game() {
       <div className="px-20 max-xl:ml-4 max-md:mr-0 max-xl:px-4 max-sm:px-0 max-sm:mx-0">
         <GameDetails key={data.gameId} data={data} />
         {commentsLoading && <Loader />}
-        <CommentBox key={data.gameId} comments={comments} />
+        <CommentsBox key={data.gameId} comments={comments} />
       </div>
     </>
   );
