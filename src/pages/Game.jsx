dd19680dab86c57157f3a90ed4b4ac1af4560062
wojtek <wswistak@student.agh.edu.pl>
2024-02-14 18:43:25 +0100

    delete conditional comments loader

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -8,13 +8,12 @@ import { useGetComments } from "../features/Game/useComment";
 function Game() {
   const { id: gameId } = useParams();
   const { data, isLoading } = useFetch(`games/${gameId}`);
-  const { commentsLoading, comments } = useGetComments(gameId);
+  const { comments } = useGetComments(gameId);
   return (
     <>
       {isLoading && <Loader />}
       <div className="px-20 max-xl:ml-4 max-md:mr-0 max-xl:px-4 max-sm:px-0 max-sm:mx-0">
         <GameDetails key={data.gameId} data={data} />
-        {commentsLoading && <Loader />}
         <CommentsBox key={data.gameId} comments={comments} />
       </div>
     </>
